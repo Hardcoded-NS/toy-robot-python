11ab5bc9053028f1e3684b4dafb1d159e9850bd2
lokulin <lauchlin@lauchlin.com>
2015-04-24 00:24:42 +1000

    Added runner logic and examples

--- toyrobot/robot.py
+++ toyrobot/robot.py
@@ -1,5 +1,5 @@
 from math import pi,sin,cos
-from .point import *
+from point import Point
 
 class Robot(object):
 
@@ -10,10 +10,10 @@ class Robot(object):
         self.dirs = dirs
 
     def left(self):
-        return self.place(self.location, self.facing - 0.5, self.table)
+        return self.place(self.location, (self.facing - 0.5)%2, self.table)
 
     def right(self):
-        return self.place(self.location, self.facing + 0.5, self.table)
+        return self.place(self.location, (self.facing + 0.5)%2, self.table)
 
     def move(self):
         return self.place(self.location + Point(sin(pi * self.facing)
@@ -22,12 +22,12 @@ class Robot(object):
 
     def report(self):
         if self.table is not None:
-            print(round(self.location.x), round(self.location.y)
-                 , self.dirs[round(self.facing)])
+            print(round(self.location.x), round(self.location.y), self.dirs[round(self.facing*2)])
+        return self
 
     def place(self, location, facing, table):
         if table is not None and table.contains(location):
-            return Robot(location, facing, table)
+            return Robot(location, facing, table, self.dirs)
         else:
             return self
 
--- toyrobot/toyrobot.py
+++ toyrobot/toyrobot.py
@@ -1,10 +1,37 @@
-from .robot import *
-from .point import *
-from .table import *
+from robot import Robot
+from point import Point
+from table import Table
+from re import compile,X
+from sys import exit,argv
 
+def main(file):
+    dirs = ["NORTH", "EAST", "SOUTH", "WEST"]
+    pattern = compile(r"""(?<=^)                                    # start
+                          (?P<cmd>MOVE$|LEFT$|RIGHT$|REPORT$|PLACE  # command
+                          (?=\s?                                    # space
+                          (?P<x>\d+),                               # x co-ord
+                          (?P<y>\d+),                               # y co-ord
+                          (?P<dir>NORTH|EAST|SOUTH|WEST)            # direction
+                          $))                                       # EOL
+                          """, X)
 
-def main():
-    dirs=["NORTH", "EAST", "SOUTH", "WEST"]
     table = Table(Point(0,0),Point(4,4))
-    robot = Robot(table = table, dirs = dirs)
-    robot.report()
+    robot = Robot(dirs = dirs)
+
+    with open(file) as f:
+        for line in f:
+            m = pattern.match(line.rstrip('\n'))
+            if m is not None and m.group("cmd") == "PLACE":
+                robot = robot.place(Point(float(m.group("x")),
+                                          float(m.group("y"))),
+                                          dirs.index(m.group("dir"))/2.0,
+                                          table)
+            elif m is not None and hasattr(robot,m.group("cmd").lower()):
+                robot = getattr(robot,m.group("cmd").lower())()
+
+
+if __name__ == '__main__':
+    if len(argv) == 2:
+        exit(main(argv[1]))
+    else:
+        print("usage: ")
